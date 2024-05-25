a04ae3323182232fd905b070663077d603ad9a0f
aimane yousr <aimaneyousr@gmail.com>
2024-05-25 16:36:06 +0100

    Add constants for window size, cell size, and frame rate

---
+++ headers/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define FALSE 0
+#define TRUE 1
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define CELL_SIZE (WINDOW_WIDTH / 24)
+#define FPS 30
+#define MY_FRAME_TARGET_TIME (1000 / FPS)
+
+int process_input();
+
+#endif /* CONSTANTS_H */
